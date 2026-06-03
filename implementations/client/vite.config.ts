1dc685faf68ae796f3bf357c718889812ce4b367
mindulle <mindullestudio@gmail.com>
2026-06-03 13:04:38 +0900

    ci: exclude playwright tests from vitest runner

--- implementations/client/vite.config.ts
+++ implementations/client/vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig(({ mode }) => {
       globals: true,
       environment: 'jsdom',
       setupFiles: './src/test/setup.ts',
+      exclude: ['**/node_modules/**', '**/dist/**', '**/tests/**'],
     },
   }
 })
